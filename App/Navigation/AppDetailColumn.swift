9ee0a654f22f13a61a387d0651fd4b09cadf19c7
gaeng2y <gaeng2y@gmail.com>
2024-04-05 01:53:53 +0900

    GridView 만드는 중

--- App/Navigation/AppDetailColumn.swift
+++ App/Navigation/AppDetailColumn.swift
@@ -10,24 +10,34 @@ import PokemonAPI
 import SwiftUI
 
 struct AppDetailColumn: View {
-    @State var pokemonEntries: [PKMPokemonEntry]? = []
+    @State var pokemonSpecies: [PKMNamedAPIResource<PKMPokemonSpecies>] = []
     let pokemonApi: PokemonApiImpl
     var region: Region?
     
+    var gridItems: [GridItem] {
+        [GridItem(.adaptive(minimum: 100), spacing: 20, alignment: .top)]
+    }
+    
     var body: some View {
-        Text("테스트")
-            .task {
-                await fetchPokemon()
+        LazyVGrid(columns: gridItems, spacing: 20) {
+            ForEach(pokemonSpecies, id: \.name) { pokemonSpecie in
+                Text(pokemonSpecie.name ?? "")
             }
-            .navigationTitle(region?.regionName ?? Region.kanto.regionName)
-            .navigationBarTitleDisplayMode(.inline)
+        }
+        .task {
+            await fetchPokemon()
+        }
+        .navigationTitle(region?.regionName ?? Region.kanto.regionName)
+        .navigationBarTitleDisplayMode(.inline)
     }
     
     private func fetchPokemon() async {
         // 1로 entries count 가져와서 species 로 이름 가져오기
         do {
-            pokemonEntries = try await pokemonApi.fetchPokedexEntries(with: 1)
-            print(pokemonEntries)
+            guard let pokemonEntries = try await pokemonApi.fetchPokedexEntries(with: 1) else {
+                return
+            }
+            pokemonSpecies = pokemonEntries.compactMap { $0.pokemonSpecies }
         } catch {
             print(error.localizedDescription)
         }
