6cc58535ec65d134119d2150d2456512e78e1923
gaeng2y <gaeng2y@gmail.com>
2024-08-29 20:27:08 +0900

    🎉 Begin project

---
+++ Project.swift
@@ -0,0 +1,34 @@
+import ProjectDescription
+
+let project = Project(
+    name: "Pokedex",
+    targets: [
+        .target(
+            name: "Pokedex",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.Pokedex",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["Pokedex/Sources/**"],
+            resources: ["Pokedex/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "PokedexTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.PokedexTests",
+            infoPlist: .default,
+            sources: ["Pokedex/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "Pokedex")]
+        ),
+    ]
+)
