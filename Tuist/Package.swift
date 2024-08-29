0c413389fabf3834481f7c4d3c3aaedd89cd3acf
gaeng2y <gaeng2y@gmail.com>
2024-08-29 20:43:54 +0900

    ➕ #4 PokemonAPI 의존성 추가
    - Tuist를 이용하여 의존성 추가

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -18,6 +18,13 @@ let package = Package(
         // Add your own dependencies here:
         // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", .upToNextMajor(from: "1.14.0"))
+        .package(
+            url: "https://github.com/pointfreeco/swift-composable-architecture",
+            .upToNextMajor(from: "1.14.0")
+        ),
+        .package(
+            url: "https://github.com/kinkofer/PokemonAPI",
+            .upToNextMajor(from: "6.1.0")
+        )
     ]
 )
